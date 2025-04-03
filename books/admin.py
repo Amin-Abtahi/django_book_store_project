1446fde258c7ad33451d532772f8280c24b88667
AMIN <aminsmaa77@gmail.com>
2025-04-03 13:02:57 +0330

    add user to book model the publisher

--- books/admin.py
+++ books/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from .models import Book, Comment
 
 
-@admin.register(Comment)
+@admin.register(Comment)    # this sends the Comment to the admin panel like this : admin.site.register(Comment, CommentAdmin)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('user', 'book', 'text','is_active','recommend', 'datetime_created')
 
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'user',)
 
-admin.site.register(Book)
+# admin.site.register(Book)
 # admin.site.register(Comment, CommentAdmin)
